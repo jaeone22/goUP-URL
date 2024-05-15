ab60f2e646ccee30a75cdf990456155ed378c31e
jaeone <jaeone22@proton.me>
2024-05-15 19:50:44 +0900

    데이터베이스에 저장하도록 변경

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const shortid = require('shortid');
 const path = require('path');
-const fs = require('fs');
+const redis = require('redis');
 const app = express();
 const port = 3000;
 
-const urlDatabasePath = path.join(__dirname, 'urlDatabase.json');
-let urlDatabase = {};
+// Redis 클라이언트 설정 (데이터베이스 주소, 포트, 비밀번호)
+const redisHost = 'svc.sel4.cloudtype.app'; // Redis 호스트 주소
+const redisPort = '30153'; // Redis 포트 (기본값은 6379)
+const redisPassword = 'hefpum-zozgum-jAspy2'; // Redis 비밀번호
 
-// 서버 시작 시 데이터베이스 로드
-if (fs.existsSync(urlDatabasePath)) {
-    const data = fs.readFileSync(urlDatabasePath);
-    urlDatabase = JSON.parse(data);
-}
+const client = redis.createClient({
+    url: `redis://${redisHost}:${redisPort}`,
+    password: redisPassword
+});
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+(async () => {
+    await client.connect();
+})();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -21,18 +28,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // URL 단축 엔드포인트
-app.post('/shorten', (req, res) => {
+app.post('/shorten', async (req, res) => {
     const originalUrl = req.body.url;
     const shortUrl = shortid.generate();
-    urlDatabase[shortUrl] = originalUrl;
-    fs.writeFileSync(urlDatabasePath, JSON.stringify(urlDatabase)); // 데이터베이스 저장
+    await client.set(`url:${shortUrl}`, originalUrl); // URL 데이터를 Redis에 저장
     res.send({ shortUrl });
 });
 
 // 단축 URL 리디렉션 엔드포인트
-app.get('/:shortUrl', (req, res) => {
+app.get('/:shortUrl', async (req, res) => {
     const shortUrl = req.params.shortUrl;
-    const originalUrl = urlDatabase[shortUrl];
+    const originalUrl = await client.get(`url:${shortUrl}`); // Redis에서 URL 데이터 가져오기
     if (originalUrl) {
         res.redirect(originalUrl);
     } else {
